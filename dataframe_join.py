5508148359b10d8c4465df67b3e1b51a7b0273f7
Leonardo Novelli <leonardo.novelli@sydney.edu.au>
2018-07-10 13:41:53 +1000

    added postprocessing and plotting scripts

---
+++ dataframe_join.py
@@ -0,0 +1,55 @@
+import os
+from datetime import datetime
+import time
+import numpy as np
+import pandas as pd
+
+# Choose whether to use FDR-corrected results or not
+fdr = False
+
+# Choose directories containing DataFrames to be merged
+traj_dir_list = [
+    os.path.join('trajectories', 'logistic_100samples_and_1000samples'),
+    os.path.join('trajectories', 'logistic_10000samples')
+]
+
+# Create output folder for the new analysis
+output_dir = os.path.join('trajectories', 'postprocessing_df_concatenate' + datetime.now().strftime("%Y_%m_%d_%Hh%Mm%Ss"), '')
+os.makedirs(output_dir)
+time.sleep(5)
+
+# Check that directories exist
+for traj_dir in traj_dir_list:
+    if os.path.exists(traj_dir):
+        print('OK: Directory found: {0}'.format(traj_dir))
+    else:
+        raise ValueError('ERROR: Directory not found: {0}'.format(traj_dir))
+
+# Load DataFrames
+df_list = []
+for traj_dir in traj_dir_list:
+    if fdr:
+        df_list.append(pd.read_pickle(os.path.join(traj_dir, 'postprocessing_fdr.pkl')))
+    else:
+        df_list.append(pd.read_pickle(os.path.join(traj_dir, 'postprocessing.pkl')))
+
+df_final = pd.DataFrame()
+for df in df_list:
+    len_before_append = len(df_final)
+    # Append partial DataFrame
+    df_final = df_final.append(df, ignore_index=True)
+    # Preserve column ordering of the partial DataFrame
+    df_final = df_final[df.columns.tolist()]
+    # Sanity check 1: the keys of the growing fianl DafaFrame should
+    # be the same as the partial DataFrame that has been appended 
+    assert set(df_final.keys()) == set(df.keys())
+    # Sanity check 2: the length of the growing fianl DafaFrame should
+    # be the sum of its length before appending the partial
+    # DataFrame + the length of the partial DataFrame
+    assert len(df_final) == len_before_append + len(df)
+
+# Save DataFrame
+if fdr:
+    df_final.to_pickle(os.path.join(output_dir, 'postprocessing_fdr.pkl'))
+else:
+    df_final.to_pickle(os.path.join(output_dir, 'postprocessing.pkl'))
